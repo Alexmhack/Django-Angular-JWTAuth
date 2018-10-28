9a38ab52e63505d71f8b42dfcd4d9459a0d7a2b0
alexHooper <alexmohack@gmail.com>
2018-10-28 15:11:50 +0530

    add api and blog urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
-from microblog.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('microblog.urls'), name='microblog'),
 ]
 
 urlpatterns += [
--- microblog/api/urls.py
+++ microblog/api/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
 
+from .views import BlogPostCreateAPIView, BlogPostAPIView
+
+urlpatterns = [
+	path('', BlogPostAPIView.as_view(), name='list'),
+	path('create/', BlogPostCreateAPIView.as_view(), name='create'),
+]
