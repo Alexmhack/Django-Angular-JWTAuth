baa24f0329e7c6657d5981a089cf50dbce08e040
alexHooper <alexmohack@gmail.com>
2018-10-28 12:06:38 +0530

    migrate database

--- microblog/models.py
+++ microblog/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class BlogPost(models.Model):
-	user = models.Foreign(User, on_delete=models.CASCADE)
-	date = models.DateTimeField(auto_add=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	date = models.DateTimeField(auto_now=True)
 	content = models.TextField()
 
 	def __str__(self):
