b7c26605b37a3a4dae5ceb4a288b43d63ef14027
alexHooper <alexmohack@gmail.com>
2018-10-28 10:42:09 +0530

    generate core module

--- microblog/frontend/src/app/app.module.ts
+++ microblog/frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
