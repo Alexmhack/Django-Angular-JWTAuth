a8b25ebc99b0865d86dabaec7f0d09639e233ef8
alexHooper <alexmohack@gmail.com>
2018-10-28 10:45:46 +0530

    generate user component

--- microblog/frontend/src/app/app.module.ts
+++ microblog/frontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
