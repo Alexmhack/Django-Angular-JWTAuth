5e346471468e6c234db0dbbd07519e31e96b0585
alexHooper <alexmohack@gmail.com>
2018-10-28 12:04:11 +0530

    create blog post model

--- microblog/models.py
+++ microblog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class BlogPost(models.Model):
+	user = models.Foreign(User, on_delete=models.CASCADE)
+	date = models.DateTimeField(auto_add=True)
+	content = models.TextField()
+
+	def __str__(self):
+		return str(self.user)
