d885005386276a8420c4d37c47948e6f8f57b8f2
alexHooper <alexmohack@gmail.com>
2018-10-28 15:12:32 +0530

    create view and serializer for blog model

--- microblog/api/serializers.py
+++ microblog/api/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
 
+from microblog.models import BlogPost
+
+class BlogPostSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = BlogPost
+		fields = '__all__'
--- microblog/api/views.py
+++ microblog/api/views.py
@@ -1,1 +1,28 @@
+from django.db.models import Q
 
+from rest_framework import generics
+from rest_framework import permissions
+
+from .serializers import BlogPostSerializer
+from microblog.models import BlogPost
+
+class BlogPostAPIView(generics.ListAPIView):
+	serializer_class = BlogPostSerializer
+
+	def get_queryset(self):
+		qs = BlogPost.objects.all().order_by("-timestamp")
+		query = self.request.GET.get('q', None)
+		if query is not None:
+			qs = qs.filter(
+				Q(content__icontains=query) |
+				Q(title__icontains=query)
+			)
+		return qs
+
+
+class BlogPostCreateAPIView(generics.CreateAPIView):
+	serializer_class = BlogPostSerializer
+	permission_classes = (permissions.IsAuthenticated,)
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
