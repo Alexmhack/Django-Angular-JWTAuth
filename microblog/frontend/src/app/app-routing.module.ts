eb8410e83dbae92559ae65ce0838f6f1b652d9c4
alexHooper <alexmohack@gmail.com>
2018-10-28 11:02:42 +0530

    add user component

--- microblog/frontend/src/app/app-routing.module.ts
+++ microblog/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UserComponent } from './user/user.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+	{ path: '', component: AppComponent },
+	{ path: 'login', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- microblog/frontend/src/app/user/user.component.ts
+++ microblog/frontend/src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../core/user.service';
+import { User } from '../shared/user';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+
+  // object represents the user for the login form
+  public user: User;
 
   ngOnInit() {
+  	this.user = {
+  		username: '',
+  		password: '',
+  	}
+  }
+
+  login(): void {
+  	this.userService.login(
+  		{ 'username': this.user.username, 'password': this.user.password }
+  	);
+  }
+
+  refreshToken(): void {
+  	this.userService.refreshToken();
+  }
+
+  logout(): void {
+  	this.userService.logout();
   }
 
 }
