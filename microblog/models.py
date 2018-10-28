2ff334ee807bc8422cbb0626e00ce7194c786c1c
alexHooper <alexmohack@gmail.com>
2018-10-28 15:11:14 +0530

    update the blog model

--- microblog/models.py
+++ microblog/models.py
@@ -5,8 +5,10 @@ User = get_user_model()
 
 class BlogPost(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	date = models.DateTimeField(auto_now=True)
+	title = models.CharField(max_length=250)
 	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now=True)
+	updated = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return str(self.user)
