00db3dd4cd071360a23d3c02ac60ec00b93f6dc0
alexHooper <alexmohack@gmail.com>
2018-10-28 11:15:35 +0530

    add private and public variables in user service

--- microblog/frontend/src/app/core/user.service.ts
+++ microblog/frontend/src/app/core/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  
+  // http options needed while sending post requests
+  private httpOptions: any;
 
-  constructor() { }
+  // actual JWT token
+  private token: string;
+
+  // JWT token expiry date
+  public tokenExpires: Date;
+
+  // username of logged in user
+  public username: string;
+
+  // errors found when logging in
+  public errors: any[] = [];
+
+  constructor(private http: HttpClient) {
+  	this.httpOptions = {
+  		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  	}
+  }
+
+
+
 }
