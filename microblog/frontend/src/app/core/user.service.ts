559a2f5d271bc7179e90f23cdeab5fb39ab9d54d
alexHooper <alexmohack@gmail.com>
2018-10-28 12:03:52 +0530

    add service for fetching user

--- microblog/frontend/src/app/core/user.service.ts
+++ microblog/frontend/src/app/core/user.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { User } from '../shared/user';
 
 @Injectable({
   providedIn: 'root'
@@ -27,11 +31,37 @@ export class UserService {
   	}
   }
 
-  login(user): void {
+  login(user) {
   	const loginUrl = '/api-token-auth/';
 
-  	this.http.post(loginUrl, JSON.stringify(user), this.httpOptions)
-  		.subscribe(data => this.updateData(data['token']));
+  	return this.http.post(loginUrl, JSON.stringify(user), this.httpOptions)
+  		.subscribe(
+  			data => {
+	  			this.updateData(data['token'])
+	  		},
+	  		err => {
+	  			this.errors = err['error'];
+	  		}
+  		)
+  }
+
+  refreshToken() {
+  	const refreshUrl = '/api-token-refresh/';
+  	this.http.post(refreshUrl, JSON.stringify({token: this.token}), this.httpOptions)
+  		.subscribe(
+  			data => {
+  				this.updateData(data['token']);
+  			},
+  			err => {
+  				this.errors = err['error'];
+  			}
+  		)
+  }
+
+  logout() {
+  	this.token = null;
+  	this.tokenExpires = null;
+  	this.username = null;
   }
 
   updateData(token): void {
@@ -40,8 +70,30 @@ export class UserService {
 
   	const tokenParts = this.token.split(/\./);
   	const tokenDecoded = JSON.parse(window.atob(tokenParts[1]));
+  	console.log(tokenDecoded);
+  	
   	this.tokenExpires = new Date(tokenDecoded * 1000);
   	this.username = tokenDecoded.username;
   }
+
+ /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }  
 
 }
